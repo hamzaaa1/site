5b2cccd0b011404cea6d79607ee46824c5f8421b
hamzaaa1 <abdourabihi10@gmail.com>
2022-06-15 14:17:40 +0100

    first change

---
+++ Home page/contact.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width , initial-scale=1.0">
+    <title>UAEDOC</title>
+    <link rel="stylesheet" href="contact.css">
+</head>
+<body>
+    <div class="contactus">
+        <div class="title">
+            <h2>Contactez-nous</h2>
+        </div>
+        <div class="box">
+            <!-- form -->
+            <div class="contact form">
+                <h3>Envoyer un message</h3>
+                <form action="">
+                    <div class="formbox">
+                        <div class="row50">
+                            <div class="inputbox">
+                                <span>Prenom</span>
+                                <input type="text" placeholder="Veuillez saisie votre prenom">
+                            </div>
+                            <div class="inputbox">
+                                <span>Nom</span>
+                                <input type="text" placeholder="Veuillez saisie votre Nom">
+                            </div>
+                            <div class="inputbox">
+                                <span>Email</span>
+                                <input type="email" placeholder="Veuillez saisie votre email">
+                            </div>
+                            <div class="inputbox">
+                                <span>Telephone</span>
+                                <input type="text" placeholder="Veuillez saisie votre numero de telephone">
+                            </div>
+                        </div>
+                        <div class="row100">
+                            <div class="inputbox">
+                                <span>Message</span>
+                               <textarea placeholder="Ecrivez votre message ici..."></textarea>
+                            </div>
+                        </div>
+                        <div class="row10">
+                            <div class="inputbox">
+                                <input type="submit" value="Envoyer">
+                            </div>
+                        </div>
+                    </div>
+                </form>
+            </div>
+            <!-- info -->
+            <div class="contact info">
+                <h3>Nous Contactez</h3>
+                <div class="infobox">
+                    <div>
+                        <span><ion-icon name="location"></ion-icon></span>
+                        <p>Quartier M'haneche II, avenue 9 Avril, B.P. 2117 Tétouan<br>Maroc</p>
+                    </div>
+                    <div>
+                        <span><ion-icon name="mail"></ion-icon></span>
+                        <a href="mailto:presidence@uae.ac.ma">presidence@uae.ac.ma</a>
+                    </div>
+                    <div>
+                        <span><ion-icon name="call"></ion-icon></span>
+                        <a href="tel:+212 539 97 93 16">+212 539 97 93 16</a>
+                    </div>
+                    <!--lien social media-->
+                    <ul class="sci">
+                        <li><a href="https://www.facebook.com/UAEofficiel"><ion-icon name="logo-facebook"></ion-icon></a></li>
+                        <li><a href="https://twitter.com/UAEofficiel"><ion-icon name="logo-twitter"></ion-icon></a></li>
+                        <li><a href="https://www.instagram.com/UAEofficiel"><ion-icon name="logo-instagram"></ion-icon></a></li>
+                        <li><a href="https://www.youtube.com/c/UAEofficiel"><ion-icon name="logo-youtube"></ion-icon></a></li>
+                    </ul>
+                </div>
+            </div>
+            <!-- map -->
+            <div class="contact map">
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3245.720842676137!2d-5.366756684774969!3d35.560595980222416!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd0b4246776e7bb7%3A0x2cf3ecdb9d5907ec!2sUniversit%C3%A9%20Abdelmalek%20Essa%C3%A2di!5e0!3m2!1sfr!2s!4v1654712305193!5m2!1sfr!2s" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            </div>
+        </div>
+    </div>
+    <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
+</body>
+</html>
---
+++ Home page/index.php
@@ -0,0 +1,31 @@
+
+<html>
+    <head>
+    <title>UAEDOC</title>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="style.css">
+    </head>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <body>
+         <div class="banner">
+             <div class="navbar">
+                 <img src="UAEDOC1.png" class="logo">
+                 <ul>
+                     <li><a href="#"><i class="fa fa-fw fa-home"></i> Acceuil</a></li>
+                     <li><a href="contact.html"><i class="fa fa-fw fa-envelope"></i> Contactez-nous</a></li>
+                     <li><a href="#"> <i class="fa fa-fw fa-search"></i> À propos</a></li>
+                     <li><a href="#"><i class="fa fa-fw fa-user"></i> Authentifier</a></li>
+                 </ul>
+             </div>
+             <div class="content">
+                 <h1>Gérer les cycles doctorales avec toute simplicité</h1>
+                 <p>Grâce à UAEDOC</p>
+                 <div>
+                    <a href="log.html"><button type="button"><span></span>Se connecter</button></a>
+                 </div>
+             </div>
+         </div> 
+         <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+         <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
+    </body>
+</html>
