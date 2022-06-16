19be01e685f589b7807dfaeeb6a4d603ff1bd9f9
hamzaaa1 <abdourabihi10@gmail.com>
2022-06-16 17:47:58 +0100

    sidbar

--- HomePage/index.php
+++ HomePage/index.php
@@ -6,7 +6,6 @@
     <link rel="stylesheet" href="style.css">
     </head>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <body>
          <div class="banner" style="background-image: linear-gradient(rgba(0,0,0,0.7),rgba(0,0,0,0.7)),url(../img/university.jpg);">
              <div class="navbar">
 
@@ -15,14 +14,14 @@
                      <li><a href="#"><i class="fa fa-fw fa-home"></i> Acceuil</a></li>
                      <li><a href="contact.php"><i class="fa fa-fw fa-envelope"></i> Contactez-nous</a></li>
                      <li><a href="#"> <i class="fa fa-fw fa-search"></i> À propos</a></li>
-                     <li><a href="#"><i class="fa fa-fw fa-user"></i> Authentifier</a></li>
+                     <li><a href="login.php"><i class="fa fa-fw fa-user"></i> Authentifier</a></li>
                  </ul>
              </div>
              <div class="content">
                  <h1>Gérer les cycles doctorales avec toute simplicité</h1>
                  <p>Grâce à UAEDOC</p>
                  <div>
-                    <a href="log.html"><button type="button"><span></span>Se connecter</button></a>
+                    <a href="../LoginPage/Login.php"><button type="button"><span></span>Se connecter</button></a>
                  </div>
              </div>
          </div> 
